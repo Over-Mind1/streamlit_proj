e0df5934be3730a49b70b94a99b86747042c7fb1
Over-Mind1 <93787511+Over-Mind1@users.noreply.github.com>
2024-07-20 23:19:22 +0300

    Add files via upload

---
+++ simple_chatbot.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import streamlit as st
+
+st.header('Customize Your Over_Mind Assistant')
+st.markdown('Made with GPT LLM')
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message('ai',avatar="🧙‍♂"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
